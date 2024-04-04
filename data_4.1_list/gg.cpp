9ad95df1dacde17a665778ad3472164244c34101
JeoungHojin <ssrosos@naver.com>
2024-04-04 23:54:52 +0900

    ADT 추가

--- data_4.1_list/gg.cpp
+++ data_4.1_list/gg.cpp
@@ -17,55 +17,95 @@ void List::Destroy() {
 }
 
 bool List::IsEmpty() {
-	if (count == 0)
+	if (Count == 0)
 		return true;
 	else
 		false;
 }
 
 bool List::IsFull() {
-	if (count == 0)
+	if (Count == 5)
 		return true;
 	else
 		false;
 }
 void List::Insert(int pos, Item I) {
-	if (IsFull() == true) {
+	if (IsFull()==true) {
 		cout << "꽉차있음" << endl;
 		return;
 	}
-	if (count == pos) { // 포짓션이랑 카운트랑 같을 때 -> 끝에 삽입되는 경우
+	if (Count == pos) { // 포지션이랑 카운트랑 같을 때 -> 끝에 삽입되는 경우
 		this->item[pos].a = I.a;
-		count++;
+		Count++;
 		cout << "리스트 끝에 삽입" << endl;
 		return;
 	}
-	if (count >= pos + 1) { //카운트가 더 클 때
+	if (Count >= pos + 1) { //카운트가 더 클 때
 		for (int i = 5 - 2;i >= pos;i++) {
 			item[i + 1].a = item[i].a;
 		}
 		item[pos].a = I.a;
-		count++;
+		Count++;
 		cout << "리스트 중간에 삽입" << endl;
 		return;
 	}
-	if (count < pos + 1) { //포지션이 더 클 때
+	if (Count < pos + 1) { //포지션이 더 클 때
 		cout << "범위 초과" << endl;
 		return;	
 	}
 }
 
 void List::Display() {
-	for (int i = 0; i = count; i++) {
-		cout << item[i].a;
-		cout << endl;
+	cout << "[";
+	for (int i = 0; i < Count; i++) {
+		cout << item[i].a << (i != Count - 1 ? ", " : "");
+	}
+	cout << "]\n";
+}
+
+void List::Del(int pos, Item I) {
+	if (IsEmpty()) {
+		cout << "이미 비어있음" << endl;
+	}
+	else {
+		if (pos == 1) {
+
+
+		}
+	}
+}
+void List::Retrive(Item I) {
+
+}
+
+
+void List::append(int item_) { //int item_을 입력받아서
+	item[Count].a = item_;//item[Count]에 item_을 추가
+	Count++;
+}
+
+int List::pop() {
+	int pop = item[Count-1].a;
+	Count--;
+	return pop;
+}
+
+void List::reverse() {
+	for (int i = 0; i < Count / 2; i++) {
+		int temp = item[i].a;
+		item[i].a = item[Count - i - 1].a;
+		item[Count - i - 1].a = temp;
 	}
 }
 
-void List::Delete(int pos) {
-	
+void List::clear() {
+	for (int i = Count-1;i == 0;i--) {
+		Count--;
+	}
 }
 
-void List::Retrive(Item I) {
-
+void List::len() {	
+	for (int i = 0;i = Count-1;i++) {
+		Count;
+	}
 }
